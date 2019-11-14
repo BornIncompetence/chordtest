7be45c525ba5e805806d59dadf1b2ab85ed919a9
Winn <winnmoo@gmail.com>
2019-11-14 14:37:15 -0800

    Moved services into project

---
+++ src/main/java/com/cecs/Services/MusicServices.java
@@ -0,0 +1,66 @@
+package com.cecs;
+
+import com.cecs.model.Music;
+import com.google.gson.GsonBuilder;
+
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MusicServices {
+    private static Music[] library;
+    private static HashMap<String, List<Music>> queries = new HashMap<>();
+
+    MusicServices() {
+    }
+
+    public static Music[] loadSongs(String asdf) {
+        if (library == null) {
+            loadLibrary();
+        }
+        return library;
+    }
+
+    public static int querySize(String query) {
+        if (query.isBlank()) {
+            return library.length;
+        }
+        return queries.get(query).size();
+    }
+
+    public static Music[] loadChunk(int start, int end, String query) {
+        if (library == null) {
+            loadLibrary();
+        }
+        if (query.isBlank()) {
+            var toIndex = Integer.min(end, library.length);
+            return Arrays.copyOfRange(library, start, toIndex);
+        }
+        if (queries.containsKey(query)) {
+            var musics = queries.get(query);
+            var toIndex = Integer.min(end, musics.size());
+            return musics.subList(start, toIndex).toArray(new Music[0]);
+        }
+        var musics = Arrays.stream(library)
+                .filter(music -> music.getArtist().toString().toLowerCase().contains(query)
+                        || music.getRelease().toString().toLowerCase().contains(query)
+                        || music.getSong().toString().toLowerCase().contains(query))
+                .collect(Collectors.toList());
+        queries.put(query, musics);
+        var toIndex = Integer.min(end, musics.size());
+        return musics.subList(start, toIndex).toArray(new Music[0]);
+
+    }
+
+    private static void loadLibrary() {
+        var reader = new InputStreamReader(App.class.getResourceAsStream("/music.json"), StandardCharsets.UTF_8);
+        var musics = new GsonBuilder().create().fromJson(reader, Music[].class);
+        for (var music : musics) {
+            music.getSong().setArtist(music.getArtist().getName());
+        }
+        library = musics;
+    }
+}
---
+++ src/main/java/com/cecs/Services/SongServices.java
@@ -0,0 +1,44 @@
+package com.cecs;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class SongServices {
+    private static final int FRAGMENT_SIZE = 16384;
+
+    SongServices() {
+    }
+
+    /*
+     * getSongChunk: Gets a chunk of a given song
+     *
+     * @param key: Song ID. Each song has a unique ID
+     *
+     * @param fragment: The chunk corresponds to [fragment * FRAGMENT_SIZE,
+     * FRAGMENT_SIZE]
+     */
+    public static String getSongChunk(String id, Long fragment) throws IOException {
+        byte[] buf = new byte[FRAGMENT_SIZE];
+
+        var filename = String.format("%s.mp3", id);
+        var inputStream = new FileInputStream(filename);
+        inputStream.skip(fragment * FRAGMENT_SIZE);
+        inputStream.read(buf);
+        inputStream.close();
+        // Encode in base64 so it can be transmitted
+        return Base64.getEncoder().encodeToString(buf);
+    }
+
+    /*
+     * getFileSize: Gets a size of the file
+     *
+     * @param key: Song ID. Each song has a unique ID
+     */
+    public static int getFileSize(String id) {
+        var filename = String.format("%s.mp3", id);
+
+        return (int) new File(filename).length();
+    }
+}
